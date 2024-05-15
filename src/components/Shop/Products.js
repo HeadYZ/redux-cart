0752816c49a139d12f01202c52cf2294403905eb
Patryk <krasowski.p@outlook.com>
2024-05-15 20:53:07 +0200

    add DUMMY PRODUCTS

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,27 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from './ProductItem'
+import classes from './Products.module.css'
 
-const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
-};
+const DUMMY_PRODUCTS = [
+	{ price: 6, title: 'My First Book', description: 'The first book i ever wrote' },
+	{ price: 5, title: 'My Second Book', description: 'The second book i ever wrote' },
+]
 
-export default Products;
+const Products = props => {
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map(product => (
+					<ProductItem
+						key={product.title}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	)
+}
+
+export default Products
