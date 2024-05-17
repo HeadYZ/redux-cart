39cc16aaa67713f5b20c43cc99fc5f91b9d52d90
Patryk <krasowski.p@outlook.com>
2024-05-17 19:51:00 +0200

    add fetch to update data on firebase after muted state in redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { useSelector } from 'react-redux'
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
+import { useEffect } from 'react'
 
 function App() {
 	const showCart = useSelector(state => state.cartReducer.cartIsVisible)
+	const cart = useSelector(state => state.cartReducer)
+
+	useEffect(() => {
+		fetch('https://practice-f07b4-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) })
+	}, [cart])
 	return (
 		<Layout>
 			{showCart && <Cart />}
