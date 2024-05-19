4d6ab17a52c5920ae0346d4d45a63281f7c2efb8
Patryk <krasowski.p@outlook.com>
2024-05-19 19:29:18 +0200

    add creator action thunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products.jsx'
 import Notification from './components/UI/Notification.jsx'
 import { useEffect } from 'react'
-import { cartActions } from './store/cart'
+import { sendCartData } from './store/cart.js'
 
 let isInitial = true
 
@@ -15,28 +15,11 @@ function App() {
 	const notification = useSelector(state => state.cartReducer.notification)
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(cartActions.showNotification({ status: 'pending', title: 'Sending ...', message: 'Sending cart data!' }))
-			const response = await fetch('https://practice-f07b4-default-rtdb.firebaseio.com/cart.json', {
-				method: 'PUT',
-				body: JSON.stringify(cart),
-			})
-			if (!response.ok) {
-				throw new Error('Sending cart data failed!')
-			}
-
-			dispatch(
-				cartActions.showNotification({ status: 'success', title: 'Success', message: 'Send cart data successfully!' })
-			)
-		}
 		if (isInitial) {
 			isInitial = false
 			return
 		}
-
-		sendCartData().catch(error => {
-			dispatch(cartActions.showNotification({ status: 'error', title: 'Error', message: 'Sending cart data failed!' }))
-		})
+		dispatch(sendCartData(cart))
 	}, [cart, dispatch])
 	return (
 		<>
